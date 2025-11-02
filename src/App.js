d8d0ea9c02cc4da94a1e541a5c85633a31d636c6
Alex Brilevski <brilevski.alex@gmail.com>
2025-11-02 15:30:37 +0300

    Users data was moved and provided via context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import UsersContext from './store/users-contex';
 import UserFinder from './components/UserFinder';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  }
   return (
     <div>
-      <UserFinder />
+      <UsersContext.Provider value={usersContext}>
+        <UserFinder />
+      </UsersContext.Provider>
     </div>
   );
 }
